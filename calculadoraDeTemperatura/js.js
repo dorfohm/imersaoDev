1364734d15025d0b125adf40ebc7b6f91593323d
dorfohm <dorfohm@hotmail.com>
2022-09-06 12:46:52 -0300

    Calculadora de temperatura finalizada

--- calculadoraDeTemperatura/js.js
+++ calculadoraDeTemperatura/js.js
@@ -2,10 +2,15 @@ var valor = document.querySelector("#valor");
 var resultado = document.querySelector("#resultado");
 var convertido = 0;
 var conversor = {
-    converter: function () {
+    converterF: function () {
         convertido = (valor.value) * 9/5 + 32;
         console.log(convertido);
         resultado.innerHTML = "A temperatura convertida em Fahrenheit é: " + convertido + "°F";
+    },
+    converterK: function () {
+        convertido = Number(valor.value) + 273.15;
+        console.log(convertido);
+        resultado.innerHTML = "A temperatura convertida em Kelvin é: " + convertido + "°K";
     }
 }
 
