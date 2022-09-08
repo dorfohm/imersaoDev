1de7e1586b56afd3acef9398a3fde78f3cef2303
dorfohm <dorfohm@hotmail.com>
2022-09-08 15:11:59 -0300

    Projeto inicial

---
+++ aluraFlix/js.js
@@ -0,0 +1,12 @@
+var listaFilmes = ["Yesterday", "A chegada", "Escola de Rock", "Harry Potter"];
+
+listaFilmes.push("Harry Potter 2");
+listaFilmes.push("Harry Potter 3");
+listaFilmes.push("Harry Potter 4");
+listaFilmes.push("Harry Potter 5");
+
+for(var i = 0; i < listaFilmes.length; i++){
+    document.write("<p>" + listaFilmes[i] + "</p>");
+}
+
+
