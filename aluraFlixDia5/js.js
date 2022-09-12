45a06f1538065b1d013866fdd1dd213853602e6e
dorfohm <dorfohm@hotmail.com>
2022-09-12 16:23:50 -0300

    Criado botão de remover, finalizado

--- aluraFlixDia5/js.js
+++ aluraFlixDia5/js.js
@@ -1,35 +1,42 @@
-var arrayFilmes = ["https://br.web.img3.acsta.net/medias/nmedia/18/85/94/86/20078371.jpg", "https://animefire.net/img/animes/claymore-large.webp?v=1", "https://images-na.ssl-images-amazon.com/images/I/51Mm5G4Z6gL._SX372_BO1,204,203,200_.jpg"];
-var arrayNomeFilmes = ["teste","teste","teste"];
+var arrayFilmes = [];
+var arrayNomeFilmes = [];
 
 const elementoListaFilmes = document.getElementById("listaFilmes");
 const elementoLinkFilme = document.getElementById("filme");
 const elementoNomeFilme = document.getElementById("nomeFilme");
-
-for (var i = 0; i < arrayNomeFilmes.length; i++) {
-    elementoListaFilmes.innerHTML += `<img src =  "${arrayFilmes[i]}">`;
-    elementoListaFilmes.innerHTML += `<p> ${arrayNomeFilmes[i]} </p>`;
-}
-
-arrayNomeFilmes.forEach(element => {
-    elementoTextoImagem.innerHTML += element
-})
+const elementoImagemFilme = document.getElementById("imagemFilme");
 
 const filmes = {
     adicionar: function () {
-        if (arrayFilmes.includes(elementoLinkFilme.value)){
-            alert("Imagem já existente");
+        if (arrayFilmes.includes(elementoLinkFilme.value) || arrayNomeFilmes.includes(elementoNomeFilme.value)){
+            alert("Imagem/Nome já existente");
             return;
-        } //Milagre
+        }
         elementoListaFilmes.innerHTML = "";
         arrayFilmes.push(elementoLinkFilme.value);
-        arrayFilmes.forEach(element => {
-            elementoListaFilmes.innerHTML += `<img src =  "${element}">`
-        })
+        arrayNomeFilmes.push(elementoNomeFilme.value);
+        arrayFilmes.forEach((filme, nome) => {
+            const nomes = arrayNomeFilmes[nome];
+            elementoListaFilmes.innerHTML += `<div id="${nomes}" class = "imagemFilmes"> <img src =  "${filme}"> <p> ${nomes} </p> </div>`
+        });
+        elementoLinkFilme.value = "";
+        elementoNomeFilme.value = "";
+    },
+    remover: function () {
+        let nomeSelecionado = prompt("Insira o nome"); 
+        if(arrayNomeFilmes.includes(nomeSelecionado)) {
+            posicaoDoNome = arrayNomeFilmes.indexOf(nomeSelecionado);
+            console.log(posicaoDoNome);
+            arrayNomeFilmes.splice(posicaoDoNome, 1);
+            arrayFilmes.splice(posicaoDoNome, 1);
+            var elem = document.getElementById(`${nomeSelecionado}`);
+            elem.remove();
+        } else {
+            alert("Nome não encontrado");
+        }
+        arrayFilmes.forEach((filme, nome) => {
+            const nomes = arrayNomeFilmes[nome];
+            elementoListaFilmes.innerHTML += `<div id="imagemFilmes" class = "imagemFilmes"> <img src =  "${filme}"> <p> ${nomes} </p> </div>`
+        });
     }
 }
-
-
-
-//Escolher o seu tema preferido para adaptar ao nosso código, ou seja, ao invés de filmes pode ser uma lista de animes, HQ's, cursos, capas de jogos... - Feito
-//Criar uma condição para não adicionar filmes repetidos, caso eles já tenham sido adicionados anteriormente - Feito
-//Criar um campo e botão para adicionar a imagem pela tela, e não direto no código - Feito
